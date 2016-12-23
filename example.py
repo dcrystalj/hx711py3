8d02c457ea2165df0dae92a69ad25b2da3dd1971
Chris M <chris.j.major+github@gmail.com>
2016-12-23 14:36:41 +0000

    Rewritten internal passing code to be independent of host system byte ordering and endianness

--- example.py
+++ example.py
@@ -9,16 +9,7 @@ def cleanAndExit():
     print("Bye!")
     sys.exit()
 
-hx = HX711(5, 6)
-
-# I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
-# Still need to figure out why does it change.
-# If you're experiencing super random values, change these values to MSB or LSB until to get more stable values.
-# There is some code below to debug and log the order of the bits and the bytes.
-# The first parameter is the order in which the bytes are used to build the "long" value.
-# The second paramter is the order of the bits inside each byte.
-# According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
-hx.set_reading_format("LSB", "MSB")
+hx = HX711(27, 17)
 
 # HOW TO CALCULATE THE REFFERENCE UNIT
 # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
--- hx711.py
+++ hx711.py
@@ -1,10 +1,9 @@
 import RPi.GPIO as GPIO
 import time
-import sys
-import numpy as np  # sudo apt-get python-np
+import numpy as np
 
 class HX711:
-    def __init__(self, dout, pd_sck, gain=128):
+    def __init__(self, dout, pd_sck, gain=128, bitsToRead=24):
         self.PD_SCK = pd_sck
         self.DOUT = dout
 
@@ -12,25 +11,15 @@ class HX711:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.PD_SCK, GPIO.OUT)
         GPIO.setup(self.DOUT, GPIO.IN)
-
         self.GAIN = 0
-        self.REFERENCE_UNIT = 1  # The value returned by the hx711 that corresponds to your reference unit AFTER dividing by the SCALE.
-
+        self.REFERENCE_UNIT = 100
         self.OFFSET = 1
         self.lastVal = 0
-
-        self.LSByte = [2, -1, -1]
-        self.MSByte = [0, 3, 1]
-
-        self.MSBit = [0, 8, 1]
-        self.LSBit = [7, -1, -1]
-
-        self.byte_range_values = self.LSByte
-        self.bit_range_values = self.MSBit
-
+        self.bitsToRead = bitsToRead
+        self.twosComplementThreshold = 1<<(bitsToRead-1)
+        self.twosComplementOffset = -(1<<(bitsToRead))
         self.set_gain(gain)
-
-        time.sleep(1)
+        self.read()
 
     def is_ready(self):
         return GPIO.input(self.DOUT) == 0
@@ -46,81 +35,40 @@ class HX711:
         GPIO.output(self.PD_SCK, False)
         self.read()
 
-    def createBoolList(self, size=8):
-        ret = []
-        for i in range(8):
-            ret.append(False)
-        return ret
-
-    def read(self):
+    def waitForReady(self):
         while not self.is_ready():
-            #print("WAITING")
             pass
 
-        dataBits = [self.createBoolList(), self.createBoolList(), self.createBoolList()]
-        dataBytes = [0x0] * 4
+    def correctTwosComplement(self,unsignedValue):
+        if ( unsignedValue >= self.twosComplementThreshold ):
+            return unsignedValue + self.twosComplementOffset
+        else:
+            return unsignedValue
 
-        for j in range(self.byte_range_values[0], self.byte_range_values[1], self.byte_range_values[2]):
-            for i in range(self.bit_range_values[0], self.bit_range_values[1], self.bit_range_values[2]):
-                GPIO.output(self.PD_SCK, True)
-                dataBits[j][i] = GPIO.input(self.DOUT)
-                GPIO.output(self.PD_SCK, False)
-            dataBytes[j] = np.packbits(np.uint8(dataBits[j]))
+    def read(self):
+        self.waitForReady()
+      
+        unsignedValue = 0;
+        for i in range(0,self.bitsToRead):
+            GPIO.output(self.PD_SCK, True)
+            bitValue = GPIO.input(self.DOUT)
+            GPIO.output(self.PD_SCK, False)
+            unsignedValue = unsignedValue << 1
+            unsignedValue = unsignedValue | bitValue
 
         #set channel and gain factor for next reading
         for i in range(self.GAIN):
             GPIO.output(self.PD_SCK, True)
             GPIO.output(self.PD_SCK, False)
+        
+        return self.correctTwosComplement( unsignedValue )
 
-        #check for all 1
-        #if all(item is True for item in dataBits[0]):
-        #    return self.lastVal
-
-        dataBytes[2] ^= 0x80
-
-        return dataBytes
-
-    def get_binary_string(self):
-        binary_format = "{0:b}"
-        np_arr8 = self.read_np_arr8()
-        binary_string = ""
-        for i in range(4):
-            # binary_segment = binary_format.format(np_arr8[i])
-            binary_segment = format(np_arr8[i], '#010b')
-            binary_string += binary_segment + " "
-        return binary_string
-
-    def get_np_arr8_string(self):
-        np_arr8 = self.read_np_arr8()
-        np_arr8_string = "[";
-        comma = ", "
-        for i in range(4):
-            if i is 3:
-                comma = ""
-            np_arr8_string += str(np_arr8[i]) + comma
-        np_arr8_string += "]";
-
-        return np_arr8_string
-
-    def read_np_arr8(self):
-        dataBytes = self.read()
-        np_arr8 = np.uint8(dataBytes)
-
-        return np_arr8
-
-    def read_long(self):
-        np_arr8 = self.read_np_arr8()
-        np_arr32 = np_arr8.view('uint32')
-        self.lastVal = np_arr32
-        time.sleep(0.2)
-
-        return self.lastVal
 
     def read_average(self, times=10):
-        return np.average([self.read_long() for _ in range(times)])
+        return np.average([self.read() for _ in range(times)])
 
     def read_average_without_bias(self, times=10, spikes=3):
-        values = sorted([self.read_long() for _ in range(times)])
+        values = sorted([self.read() for _ in range(times)])
 
         return np.average(values[spikes:-spikes])
 
@@ -141,7 +89,6 @@ class HX711:
         return value
 
     def tare(self, times=25):
-
         # Backup REFERENCE_UNIT value
         reference_unit = self.REFERENCE_UNIT
         self.set_reference_unit(1)
@@ -150,18 +97,7 @@ class HX711:
         self.set_offset(value)
 
         self.set_reference_unit(reference_unit)
-        time.sleep(1)
-
-    def set_reading_format(self, byte_format="LSB", bit_format="MSB"):
-        if byte_format == "LSB":
-            self.byte_range_values = self.LSByte
-        elif byte_format == "MSB":
-            self.byte_range_values = self.MSByte
-
-        if bit_format == "LSB":
-            self.bit_range_values = self.LSBit
-        elif bit_format == "MSB":
-            self.bit_range_values = self.MSBit
+        time.sleep(0.1)
 
     def set_offset(self, offset):
         self.OFFSET = offset
